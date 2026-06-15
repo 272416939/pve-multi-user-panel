d785b5f403b634955c6b0ea3127dbd4b69d6abd4
Allen528 <272416939@qq.com>
2026-06-15 22:07:04 +0800

    chore: test/加入gitignore

--- test/dashboard-vue-dollar.test.js
+++
@@ -1,9 +0,0 @@
-var { expect } = require('chai');
-var fs = require('fs');
-
-describe('dashboard.html Vue 模板中 $ 使用规范', function() {
-  it('LXC续费按钮不应使用 $.openRenewModal（Vue模板中 $ 是组件实例）', function() {
-    var html = fs.readFileSync('e:\\code\\pve管理面板\\public\\dashboard.html', 'utf8');
-    expect(html).to.not.match(/\$\.openRenewModal/);
-  });
-});
